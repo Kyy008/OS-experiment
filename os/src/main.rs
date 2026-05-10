7518a9fc3e8e57a9921e97afba8ac382fbd7bb31
kyy008 <keyuyangunis@126.com>
2026-05-11 04:26:46 +0800

    feat: complete main.rs -23301037 客昱阳

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,10 +1,76 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::{self, Write};
 use core::panic::PanicInfo;
+use core::arch::asm;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+const SYSCALL_EXIT: usize = 93;
+const SYSCALL_WRITE: usize = 64;
+
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret: isize;
+    unsafe {
+        asm!(
+            "ecall",
+            in("x10") args[0],
+            in("x11") args[1],
+            in("x12") args[2],
+            in("x17") id,
+            lateout("x10") ret
+        );
+    }
+    ret
+}
+
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+}
+
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::print(format_args!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! println {
+    () => {
+        $crate::print!("\n");
+    };
+    ($($arg:tt)*) => {
+        $crate::print!("{}\n", format_args!($($arg)*));
+    };
+}
+
+
+#[unsafe(no_mangle)]
+extern "C" fn _start() -> ! {
+    println!("Hello, world!");
+    sys_exit(9);
     loop {}
 }
 
